b65636d8382d64d996ba7fead3517f5b20961416
Mihail Belozerov <miha_bmv_@mail.ru>
2026-08-27 16:46:28 +0600

    refactoring, new class DrawingsList

--- test/test_DrawingsList.cpp
+++ test/test_DrawingsList.cpp
@@ -37,52 +37,49 @@ private slots:
         QCOMPARE(ld.size(), 5);
     }
 
-    // void testRemove() {
-    //     QCOMPARE(ld.remove("05.11.2021"), true);
-    //     QCOMPARE(ld.size(), 4);
+    void testRemove() {
+        QCOMPARE(ld.remove(Drawing("ИМЭ-987", "Поршень")), true);
+        QCOMPARE(ld.size(), 4);
 
-    //     QCOMPARE(ld.remove("25.09.2008"), false);
-    //     QCOMPARE(ld.size(), 4);
+        QCOMPARE(ld.remove(Drawing("ИМЭ-987", "Поршень")), false);
+        QCOMPARE(ld.size(), 4);
 
-    //     QCOMPARE(ld.remove("18.03.2020"), true);
-    //     QCOMPARE(ld.size(), 3);
+        QCOMPARE(ld.remove(Drawing("1-234", "Башмак")), true);
+        QCOMPARE(ld.size(), 3);
+    }
 
-    //     QCOMPARE(ld.remove("55.08.2019"), false);
-    // }
+    void testInsertWithQueue() {
+        QCOMPARE(ld.insert(Drawing("СЗ-0101", "Тарелка")), 4);
+        QCOMPARE(ld.size(), 4);
 
-    // void testInsertWithQueue() {
-    //     QCOMPARE(ld.insert("13.06.2015"), 4);
-    //     QCOMPARE(ld.size(), 4);
+        QCOMPARE(ld.insert(Drawing("ИМЭ-123", "Блок")), 3);
+        QCOMPARE(ld.size(), 5);
 
-    //     QCOMPARE(ld.insert("26.03.2018"), 3);
-    //     QCOMPARE(ld.size(), 5);
+        QCOMPARE(ld.insert(Drawing("1-9876", "Фланец")), 6);
+        QCOMPARE(ld.size(), 6);
+    }
 
-    //     QCOMPARE(ld.insert("09.01.2019"), 6);
-    //     QCOMPARE(ld.size(), 6);
-    // }
+    void testGetId() {
+        QCOMPARE(ld.getID(Drawing("СЗ-0101", "Тарелка")), 4);
+        QCOMPARE(ld.getID(Drawing("111-11", "Шкиф")), 2);
+        QCOMPARE(ld.getID(Drawing("ИМЭ-123", "Блок")), 3);
+        QCOMPARE(ld.getID(Drawing("987-789", "Корпус")), 5);
+        QCOMPARE(ld.getID(Drawing("1-9876", "Фланец")), 6);
+        QCOMPARE(ld.getID(Drawing("ИМН-111", "Колесо рабочее")), 1);
 
-    // void testGetId() {
-    //     QCOMPARE(ld.getID("13.06.2015"), 4);
-    //     QCOMPARE(ld.getID("15.07.2024"), 2);
-    //     QCOMPARE(ld.getID("26.03.2018"), 3);
-    //     QCOMPARE(ld.getID("28.08.2019"), 5);
-    //     QCOMPARE(ld.getID("09.01.2019"), 6);
-    //     QCOMPARE(ld.getID("25.08.2025"), 1);
+        QCOMPARE(ld.getID(Drawing("111-222", "Звездочка")), 0);
+    }
 
-    //     QCOMPARE(ld.getID("01.05.2017"), 0);
-    //     QCOMPARE(ld.getID("55.05.2017"), -1);
-    // }
+    void testGetDate() {
+        QCOMPARE(ld.getDrawing(4), Drawing("СЗ-0101", "Тарелка"));
+        QCOMPARE(ld.getDrawing(2), Drawing("111-11", "Шкиф"));
+        QCOMPARE(ld.getDrawing(3), Drawing("ИМЭ-123", "Блок"));
+        QCOMPARE(ld.getDrawing(5), Drawing("987-789", "Корпус"));
+        QCOMPARE(ld.getDrawing(6), Drawing("1-9876", "Фланец"));
+        QCOMPARE(ld.getDrawing(1), Drawing("ИМН-111", "Колесо рабочее"));
 
-    // void testGetDate() {
-    //     QCOMPARE(ld.getDate(4), "13.06.2015");
-    //     QCOMPARE(ld.getDate(2), "15.07.2024");
-    //     QCOMPARE(ld.getDate(3), "26.03.2018");
-    //     QCOMPARE(ld.getDate(5), "28.08.2019");
-    //     QCOMPARE(ld.getDate(6), "09.01.2019");
-    //     QCOMPARE(ld.getDate(1), "25.08.2025");
-
-    //     QCOMPARE(ld.getDate(7), "");
-    // }
+        QCOMPARE(ld.getDrawing(7), Drawing());
+    }
 };
 
 QTEST_APPLESS_MAIN(TestDrawingsList)
